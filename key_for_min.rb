78148a129fd19f652a1289e6193a564e281ea7b5
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-14 20:24:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,4 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
+hash = {:blake => 500, :ashley => 2, :adam => 1}
+key_for_min_value(hash)
 end
